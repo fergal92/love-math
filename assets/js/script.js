e0c260c8348088e5190c4e3c1ccdf6c2d6f05400
Fergal Mulligan <fshaq4@gmail.com>
2024-05-08 19:59:53 +0000

    Add random number generator to runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// wait fpor the DOM to finish loading before running the game
+// wait for the DOM to finish loading before running the game
 //get the button elements and add event listners to them
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -12,21 +12,31 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert("You clicked submit!");
             } else{
                 let gameType = this.getAttribute("data-type");
-                // tell the user what button had been clicked
-                alert(`You clicked ${gameType}!`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame('addition');
+
 })
 // a docstring is used to describe function. should go right above where the function name is declared, hovering over where the function is called will provide the doc string description
 /** 
  * The main game "loop, called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if(gameType === 'addition'){
+        displayAdditionQuestion(num1, num2);
+    } else{
+        alert(`unknown game type: ${gameType}`);
+        // throw statement to stop game from running
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){
@@ -44,8 +54,10 @@ function incrementWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion(){
