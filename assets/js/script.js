c4c420e498cc8e362a1c9a3daf4edb862ab061b9
Fergal Mulligan <fshaq4@gmail.com>
2024-05-08 19:08:43 +0000

    Add button alert js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// wait fpor the DOM to finish loading before running the game
+//get the button elements and add event listners to them
+
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons = document.getElementsByTagName('button');
+
+   // for (i=0; i<buttons.length; i)++
+    for (let button of buttons){
+        button.addEventListener('click', function(){
+            // inside the code block "this" refers to the button that was just clicked, can be addition button, minus button etc.
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit!");
+            } else{
+                let gameType = this.getAttribute("data-type");
+                // tell the user what button had been clicked
+                alert(`You clicked ${gameType}!`);
+            }
+        })
+    }
+})
+
 function runGame(){
 
 }
