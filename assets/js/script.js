fa4de1f68d36f09741a05433541ff4cc99dee4a9
Fergal Mulligan <fshaq4@gmail.com>
2024-05-09 19:45:10 +0000

    Update score counting functions working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,8 +51,10 @@ function checkAnswer(){
 
     if (isCorrect){
         alert("Hey, You got it right! :)");
+        incrementScore();
     } else{
         alert(`You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -77,12 +79,23 @@ function calculateCorrectAnswer(){
     }
 
 }
+
+/**
+ * gets the current score from the DOM and increments it by 1
+ */
 function incrementScore(){
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
